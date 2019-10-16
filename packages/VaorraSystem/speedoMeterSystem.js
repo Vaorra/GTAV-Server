dbd50117076b97da126ef95b3d2d42ac6072e408
Jovin Risch <jovris@intnet.ch>
2019-10-16 18:26:58 +0200

    fixing

--- packages/VaorraSystem/speedoMeterSystem.js
+++ packages/VaorraSystem/speedoMeterSystem.js
@@ -2,11 +2,11 @@
 function playerEnterVehicleHandler(player, vehicle, seat) {
     player.outputChatBox(player.name + " got into the car with ID: " + vehicle.id + ". Seat: " + seat);
     if (seat === -1)
-        player.call("playerIsOnDriverSeat", []);
+        player.call("playerIsOnDriverSeat");
 }
 mp.events.add("playerEnterVehicle", playerEnterVehicleHandler);
 function playerExitVehicleHandler(player, vehicle) {
-    player.outputChatBox(player.name + " when out from vehicle with ID: " + vehicle.id);
-    player.call("playerLeaveDriverSeat", []);
+    player.outputChatBox(player.name + " went out from vehicle with ID: " + vehicle.id);
+    player.call("playerLeaveDriverSeat");
 }
 mp.events.add("playerExitVehicle", playerExitVehicleHandler);
--- packages/VaorraSystem/speedoMeterSystem.ts
+++ packages/VaorraSystem/speedoMeterSystem.ts
@@ -1,7 +1,7 @@
 function playerEnterVehicleHandler(player:any, vehicle:any, seat:any) {
     player.outputChatBox(`${player.name} got into the car with ID: ${vehicle.id}. Seat: ${seat}`);
     if (seat === -1)
-        player.call("playerIsOnDriverSeat", []);
+        player.call("playerIsOnDriverSeat");
 }
  
 mp.events.add("playerEnterVehicle", playerEnterVehicleHandler);
@@ -12,7 +12,7 @@ mp.events.add("playerEnterVehicle", playerEnterVehicleHandler);
 
  function playerExitVehicleHandler(player:any, vehicle:any) {
     player.outputChatBox(`${player.name} went out from vehicle with ID: ${vehicle.id}`);
-    player.call("playerLeaveDriverSeat", []);
+    player.call("playerLeaveDriverSeat");
 }
 
 mp.events.add("playerExitVehicle", playerExitVehicleHandler);
