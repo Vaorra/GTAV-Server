982b74a91ce7aaee962dfddb9dd94e81db098436
Trexon25 <trexon@gmx.ch>
2020-05-18 18:29:14 +0200

    changed project structure

--- packages/AdminTools/static.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prefix = "!{#343a40}[!{#45e4a3}Vaorra!{#343a40}] !{#ffffff}";
-exports.spawnPosition = new mp.Vector3(-427.5, 1116, 326.8);
